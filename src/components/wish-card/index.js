e035d33d7707b8ee30d51a1ea66274a0bfa24039
GolotaMishka <golotamm@gmail.com>
2022-08-30 18:44:59 +0300

    fix: tried to fix fonts

--- src/components/wish-card/index.js
+++ src/components/wish-card/index.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { StaticImage } from "gatsby-plugin-image";
-// import cx from "classNames";
 import "./styles.css";
 
 const WishCard = ({ title, description, className }) => {
@@ -11,6 +10,7 @@ const WishCard = ({ title, description, className }) => {
         alt={`Image ilustration of ${title} `}
         src="https://images.unsplash.com/photo-1615445969492-6894df4a5613?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80"
       />
+
       <h2 className="wishTitle">{title}</h2>
       <p className="wishDescription">{description}</p>
     </div>
