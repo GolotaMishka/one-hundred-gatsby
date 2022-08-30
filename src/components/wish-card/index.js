3cdf6436f39038ab9cff0fcfd0f255ff510d206c
GolotaMishka <golotamm@gmail.com>
2022-08-30 15:27:37 +0300

    fix: build

--- src/components/wish-card/index.js
+++ src/components/wish-card/index.js
@@ -1,13 +1,13 @@
 import * as React from "react";
 import { StaticImage } from "gatsby-plugin-image";
-import cx from "classNames";
+// import cx from "classNames";
 import "./styles.css";
 
 const WishCard = ({ title, description, className }) => {
   return (
     <div className="card">
       <StaticImage
-        className={cx("wishImage", className)}
+        className={`wishImage ${className}`}
         alt={`Image ilustration of ${title} `}
         src="https://images.unsplash.com/photo-1615445969492-6894df4a5613?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80"
       />
